a67195c7e7c20c7af147af86a04bf3eae6847d97
alirafli <mrafli1212@gmail.com>
2022-05-18 23:40:39 +0700

    dbconfig3

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,5 +3,5 @@ module.exports = {
   USER: "jgnqwgflltboim",
   PASSWORD: "638bda40e5f8bb0fadf526ecc7dee47778832eb40ad5480020d711f847952b03",
   DB: "d6h3supp2orrkp",
-  dialect: "mysql",
+  dialect: "postgres",
 };
