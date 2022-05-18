0d736eb6c9c7be359acaa02810c9f87c78fb9c4c
alirafli <mrafli1212@gmail.com>
2022-05-18 23:31:12 +0700

    dbconfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "dental_corner",
+  HOST: "ec2-34-201-95-176.compute-1.amazonaws.com",
+  USER: "jgnqwgflltboim",
+  PASSWORD: "638bda40e5f8bb0fadf526ecc7dee47778832eb40ad5480020d711f847952b03",
+  DB: "d6h3supp2orrkp",
   dialect: "mysql",
 };
